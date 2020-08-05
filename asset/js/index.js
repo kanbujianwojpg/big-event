47436fe7b52e98fcacb90b5bbe1083397033c4b9
kanbujianwojpg <1468057882@qq.com>
2020-08-05 11:09:11 +0800

    获取并展示用户信息

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,2 +1,33 @@
 //控制主页
-$(function () {});
+$(function () {
+  $.ajax({
+    type: 'get',
+    url: '/my/userinfo',
+    headers: {
+      Authorization: sessionStorage.getItem('mytoken'),
+    },
+    success: function (res) {
+      if (res.status === 0) {
+        var data = res.data;
+        console.log(data);
+        var user = $('.welcome');
+        user.find('.avatar').hide();
+        if (data.user_pic) {
+          user.html(`<img src="${data.user_pic}" />`);
+        } else {
+          user.html(`<div class='avatar'>U</div>`);
+        }
+        user.append(`<span>欢迎${data.username}</span>`);
+        //更新右上角
+        var user = $('.nav-user');
+        user.find('.avatar').hide();
+        if (data.user_pic) {
+          user.html(`<img src="${data.user_pic}" />`);
+        } else {
+          user.html(`<div class='avatar'>U</div>`);
+        }
+        user.append(`<span>${data.username}</span>`);
+      }
+    },
+  });
+});
