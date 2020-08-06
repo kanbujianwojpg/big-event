579c8ac22898423f037c3530d80aa9b63c06fa43
kanbujianwojpg <1468057882@qq.com>
2020-08-06 09:53:13 +0800

    获取用户基本信息

---
+++ asset/js/my/base-info.js
@@ -0,0 +1,19 @@
+$(function () {
+  //调用接口获取用户信息
+  function loadUserInfo() {
+    $.ajax({
+      type: 'get',
+      url: '/my/userinfo',
+      success: function (res) {
+        if (res.status === 0) {
+          var data = res.data;
+          //把获取到的数据填充到表单
+          $('.layui-form input[name=username]').val(data.username);
+          $('.layui-form input[name=nickname]').val(data.nickname);
+          $('.layui-form input[name=email]').val(data.email);
+        }
+      },
+    });
+  }
+  loadUserInfo();
+});
