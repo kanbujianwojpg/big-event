f6f8eb77553385bae6a4a87274c3574ccdfb133d
kanbujianwojpg <1468057882@qq.com>
2020-08-06 09:56:08 +0800

    获取用户䩿信息JQ

--- asset/js/my/base-info.js
+++ asset/js/my/base-info.js
@@ -7,7 +7,7 @@ $(function () {
       success: function (res) {
         if (res.status === 0) {
           var data = res.data;
-          //把获取到的数据填充到表单
+          //把获取到的数据填充到表单JQ方式
           $('.layui-form input[name=username]').val(data.username);
           $('.layui-form input[name=nickname]').val(data.nickname);
           $('.layui-form input[name=email]').val(data.email);
