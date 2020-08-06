b51f3c5b697cd3c6e2ff1c127b26c52659810cb8
kanbujianwojpg <1468057882@qq.com>
2020-08-06 11:19:53 +0800

    重置密码提交表单

--- asset/js/my/base-repwd.js
+++ asset/js/my/base-repwd.js
@@ -1,3 +1,17 @@
-$(function() {
-  
-})
+$(function () {
+  $('.layui-form').submit(function (e) {
+    e.preventDefault();
+    //获取表单数据
+    var fd = $(this).serialize();
+    console.log(fd);
+    //调用接口进行密码更新
+    $.ajax({
+      type: 'post',
+      url: '/my/updatepwd',
+      data: fd,
+      success: function (res) {
+        layer.msg(res.message);
+      },
+    });
+  });
+});
