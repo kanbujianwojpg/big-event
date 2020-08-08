34f76124153682b3ec6fe16b9a37c2314a3bcbfd
kanbujianwojpg <1468057882@qq.com>
2020-08-08 12:38:22 +0800

    编辑功能

--- asset/js/article/category.js
+++ asset/js/article/category.js
@@ -1,4 +1,5 @@
 $(function () {
+  var form = layui.form;
   function loadArticle() {
     $.ajax({
       type: 'get',
@@ -58,6 +59,55 @@ $(function () {
         },
       });
       layer.close(index);
+    });
+  });
+  //编辑操作
+  var editIndex = null;
+  $('.layui-table tbody').on('click', '.edit', function (e) {
+    //获取要编辑的分类的id
+    var id = $(e.target).data('id');
+    //根据id查询分类数据
+    $.ajax({
+      type: 'GET',
+      url: '/my/article/cates/' + id,
+      data: {
+        id: id,
+      },
+      success: function (res) {
+        if (res.status === 0) {
+          //关闭弹窗刷新列表
+          editIndex = layer.open({
+            //弹出层类型
+            type: 1,
+            //弹出层标题
+            title: '修改文章分类',
+            //弹出层内容
+            content: $('#edit-form-tpl').html(),
+            //弹出层区域大小
+            area: ['500px', '250px'],
+          });
+          //填充表单数据
+          form.val('edit-form', res.data);
+          //监听submit事件,提交表单
+          $('#edit-form').submit(function (e) {
+            e.preventDefault();
+            console.log($(this).serialize());
+            $.ajax({
+              type: 'post',
+              url: '/my/article/updatecate',
+              data: $(this).serialize(),
+              success: function (res) {
+                if (res.status === 0) {
+                  //关闭弹窗刷新列表
+                  layer.msg(res.message);
+                  layer.close(editIndex);
+                  loadArticle();
+                }
+              },
+            });
+          });
+        }
+      },
     });
   });
 });
